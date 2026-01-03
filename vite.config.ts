a0f9887d01a442ad6aae5e13ff50a108e0c829bd
gustavorochaC <gustavorocarvalho@hotmail.com>
2026-01-03 14:33:54 -0300

    build: adiciona build para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/ct-e-to-excel/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
