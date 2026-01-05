e889b24f28202aaa1a4106ccbb1776d7465bc514
gustavorochaC <gustavorocarvalho@hotmail.com>
2026-01-05 08:50:14 -0300

    se essa porr n for eu me mato

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "development" ? "/" : "/ct-e-to-excel/",
   build: {
     outDir: "../",
     emptyOutDir: false,
