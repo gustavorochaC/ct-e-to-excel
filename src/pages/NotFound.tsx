df6aa65a4da45c271cfad9cf1c4c011542590833
gustavorochaC <gustavorocarvalho@hotmail.com>
2026-01-05 09:02:44 -0300

    Odeio Router

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,10 +5,10 @@ const NotFound = () => {
   const location = useLocation();
 
   useEffect(() => {
-    console.error("404 Error: User attempted to access non-existent route:", location.pathname);
-    // #region agent log
-    fetch('http://127.0.0.1:7242/ingest/730c0d06-4f93-497d-8fb0-19604a532633',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({location:'NotFound.tsx:useEffect',message:'404 error triggered',data:{pathname:location.pathname,search:location.search,hash:location.hash,windowPathname:window.location.pathname,windowHref:window.location.href,viteBase:import.meta.env.BASE_URL},timestamp:Date.now(),sessionId:'debug-session',runId:'run1',hypothesisId:'D'})}).catch(()=>{});
-    // #endregion
+    console.error(
+      "404 Error: User attempted to access non-existent route:",
+      location.pathname
+    );
   }, [location.pathname]);
 
   return (
